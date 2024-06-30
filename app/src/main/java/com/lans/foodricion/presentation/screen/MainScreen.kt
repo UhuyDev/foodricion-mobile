b9e3781cc49c5c222299403c38d2f44e8733b735
Lana <59499676+Lanzkuy@users.noreply.github.com>
2024-07-01 00:32:30 +0700

    :lipstick: add main screen

--- app/src/main/java/com/lans/foodricion/presentation/screen/MainScreen.kt
+++ app/src/main/java/com/lans/foodricion/presentation/screen/MainScreen.kt
@@ -1,1 +1,45 @@
 package com.lans.foodricion.presentation.screen
+
+import android.app.Activity
+import androidx.activity.compose.BackHandler
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.lans.foodricion.presentation.component.bottom_navigation.BottomNavigationBar
+import com.lans.foodricion.presentation.navigation.MainRoute
+import com.lans.foodricion.presentation.navigation.graph.MainNavGraph
+import com.lans.foodricion.presentation.screen.signin.SignInScreen
+
+@Composable
+fun MainScreen(
+    rootNavController: NavHostController
+) {
+    val mainNavController = rememberNavController()
+    val activity = LocalContext.current as Activity
+    val navBackStackEntry by mainNavController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination
+    BackHandler {
+        if (currentRoute?.route == MainRoute.HomeScreen.route) {
+            activity.finish()
+        }
+    }
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar(
+                navigateToHome = { mainNavController.navigate(MainRoute.HomeScreen.route) },
+                navigateToAskBot = { mainNavController.navigate(MainRoute.AskBotScreen.route) },
+                navigateToProfile = { mainNavController.navigate(MainRoute.ProfileScreen.route) }
+            )
+        }
+    ) { paddingValues ->
+        MainNavGraph(
+            rootNavController = rootNavController,
+            mainNavController = mainNavController,
+            innerPadding = paddingValues
+        )
+    }
+}
