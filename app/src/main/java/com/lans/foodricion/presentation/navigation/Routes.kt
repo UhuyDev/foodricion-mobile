92e33b1467555a633a1abe37e290be904784c22a
Lana <59499676+Lanzkuy@users.noreply.github.com>
2024-07-01 00:32:14 +0700

    :beers: add route and graph

--- app/src/main/java/com/lans/foodricion/presentation/navigation/Routes.kt
+++ app/src/main/java/com/lans/foodricion/presentation/navigation/Routes.kt
@@ -1,2 +1,19 @@
 package com.lans.foodricion.presentation.navigation
 
+object NavGraph {
+    const val RootGraph = "rootGraph"
+    const val AuthGraph = "authGraph"
+    const val MainGraph = "mainGraph"
+}
+
+sealed class AuthRoute(val route: String) {
+    object SignInScreen: AuthRoute("signIn")
+    object SignUpScreen: AuthRoute("signUp")
+    object ForgotScreen: AuthRoute("forgot")
+}
+
+sealed class MainRoute(val route: String) {
+    object HomeScreen: MainRoute("home")
+    object AskBotScreen: MainRoute("askBot")
+    object ProfileScreen: MainRoute("profile")
+}
---
+++ app/src/main/java/com/lans/foodricion/presentation/navigation/graph/AuthNavGraph.kt
@@ -0,0 +1,55 @@
+package com.lans.foodricion.presentation.navigation.graph
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.lans.foodricion.presentation.navigation.AuthRoute
+import com.lans.foodricion.presentation.navigation.NavGraph
+import com.lans.foodricion.presentation.screen.forgot_password.ForgotPasswordScreen
+import com.lans.foodricion.presentation.screen.signin.SignInScreen
+import com.lans.foodricion.presentation.screen.signup.SignUpScreen
+
+fun NavGraphBuilder.authNavGraph(
+    navController: NavController
+) {
+    navigation(
+        route = NavGraph.AuthGraph,
+        startDestination = AuthRoute.SignInScreen.route
+    ) {
+        composable(route = AuthRoute.SignInScreen.route) {
+            SignInScreen(
+                navigateToSignUp = {
+                    navController.navigate(route = AuthRoute.SignUpScreen.route) {
+                        popUpTo(route = AuthRoute.SignInScreen.route)
+                    }
+                },
+                navigateToForgotPassword = {
+                    navController.navigate(route = AuthRoute.ForgotScreen.route) {
+                        popUpTo(route = AuthRoute.ForgotScreen.route)
+                    }
+                },
+                navigateToHome = {
+                    navController.navigate(route = NavGraph.MainGraph) {
+                        popUpTo(route = AuthRoute.SignInScreen.route)
+                    }
+                }
+            )
+        }
+        composable(route = AuthRoute.SignUpScreen.route) {
+            SignUpScreen(
+                navigateToSignIn = {
+                    navController.navigateUp()
+                }
+            )
+        }
+        composable(route = AuthRoute.ForgotScreen.route) {
+            ForgotPasswordScreen(
+                navigateToSignIn = {
+                    navController.navigateUp()
+                }
+            )
+        }
+    }
+}
