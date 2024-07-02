9a58f781daf638d05abea2133aaa5c822d578178
Lana <59499676+Lanzkuy@users.noreply.github.com>
2024-07-03 01:06:18 +0700

    :construction: setup splashscreen loading

--- app/src/main/java/com/lans/foodricion/presentation/MainViewModel.kt
+++ app/src/main/java/com/lans/foodricion/presentation/MainViewModel.kt
@@ -1,9 +1,42 @@
 package com.lans.foodricion.presentation
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.lans.foodricion.data.Resource
+import com.lans.foodricion.domain.usecase.IsAuthenticatedUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-): ViewModel()
+    private val isAuthenticatedUseCase: IsAuthenticatedUseCase
+): ViewModel() {
+    private val _isAuthenticated = MutableStateFlow<Boolean?>(null)
+    val isAuthenticated: Flow<Boolean?> get() = _isAuthenticated
+    var splashState by mutableStateOf(true)
+
+    init {
+        viewModelScope.launch {
+            isAuthenticatedUseCase.invoke().collect { response ->
+                when (response) {
+                    is Resource.Success -> {
+                        _isAuthenticated.value = response.data
+                        splashState = false
+                    }
+
+                    is Resource.Error -> {
+                        _isAuthenticated.value = false
+                    }
+
+                    else -> Unit
+                }
+            }
+        }
+    }
+}
